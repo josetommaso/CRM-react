fa0c44e9b8e68c14041579954d90b10fd4a008c3
Jose Tommaso <tommaso_j@hotmail.com>
2022-10-02 20:58:39 +0100

    Navigation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,35 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 const Layout = () => {
+  const location = useLocation();
+
+  console.log(location.pathname);
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
         <h2 className="text-4xl font-black text-white text-center">
           CRM - Clients
         </h2>
+        <nav className="mt-10">
+          <Link
+            to="/"
+            className={`${
+              location.pathname === '/' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300 transition-colors duration-500`}
+          >
+            Clients
+          </Link>
+          <Link
+            to="/clients/new"
+            className={`${
+              location.pathname === '/clients/new'
+                ? 'text-blue-300'
+                : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300 transition-colors duration-500`}
+          >
+            Create Client
+          </Link>
+        </nav>
       </aside>
       <main className="md:w-3/4 p-10 md:min-h-screen overflow-scroll">
         <Outlet />
